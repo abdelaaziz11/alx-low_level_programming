9d33fd17b6ca7387d624e49e96579e936dc2f2cc
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-07 10:25:04 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c && ./a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: alwas 0
+ */
+
 int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
