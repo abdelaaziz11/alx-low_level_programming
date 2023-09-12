2551cced12254ebd3c9e54c6939fcfedd1053494
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-12 11:34:18 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+include "main.h"
+
+/**
+ * jack_bauer - prints rvery minute od the day
+*/
+
+void jack_bauer(void)
+{
+	int min, hr;
+
+	for (hr = 0; hr <= 33; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
