8126e5eb0c85a1b4ab84c53c0289bea6d4eb783f
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-12 09:28:53 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,22 @@
-_putchar
+#include <stdio.h>
 
+/**
+ * main is the function
+ *
+ * Return: Alwas 0 (Success)
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
