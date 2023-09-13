4c72a53f6f71e59634a6b8f19e293d8d47c2fe7d
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-12 18:31:26 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,42 +7,42 @@
 
 void print_times_table(int n)
 {
-	int num, mult, prod;
+	int i, w, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (num = 0; num <= n; num++)
+		for (i = 0; i <= n; i++)
 		{
-			for (mult = 1; mult <= n; mult++)
+			for (w = 1; w <= n; w++)
 			{
-				prod = num * mult;
-				if (mult == 0)
+				k = w * i;
+				if (w == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && w != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(prod + '0');
-				} else if (prod < 10 && mult != 0)
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(prod + '0');
-				} else if (prod >= 10 && prod < 100)
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar((prod / 10) + '0');
-					_putchar((prod % 10) + '0');
-				} else if (prod >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10) % 10) + '0');
-					_putchar((prod % 10) + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
-			_putchar("\n");
+			_putchar('\n');
 		}
 	}
 
