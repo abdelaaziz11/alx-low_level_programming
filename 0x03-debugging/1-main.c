5e7e6df27bd544413a0755dd9d72ef15113f3f52
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-13 08:59:22 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,15 +13,9 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10);*/
-	/*
-	 * this
-	 * is
-	 * a
-	 * multi line
-	 * comment
-	 */
-	/*single line comments*/
+	/*while (i < 10;*/
+	/*{*/
+	/*}*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
