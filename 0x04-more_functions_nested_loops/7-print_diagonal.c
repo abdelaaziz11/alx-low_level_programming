2abd522475aea8e4375400d254418a0e98669f44
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-14 08:17:09 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,6 +19,8 @@ void print_diagonal(int n)
 	{
 	for (space = 0; space < n; space++)
 	{
+	_putchar(' ');
+	_putchar(92);/*is equal to '/' char*/
 	if (space == postn)
 	_putchar('\\');
 	else if (space < postn)
