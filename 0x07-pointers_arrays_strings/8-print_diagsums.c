109c91e10c36ac31073422a099b6e02613d61a43
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-25 05:27:50 -0700

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - description
+ * @a: integer of pointer input
+ * @size: integer of pointer input
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+
+}
