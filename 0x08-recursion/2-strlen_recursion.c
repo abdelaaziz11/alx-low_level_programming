428a26b9818febb7c66ee038a541066a327da26b
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-28 13:43:04 -0700

    0x08

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,11 +11,16 @@
 
 int _strlen_recursion(char *s)
 {
-	int len = 0;	
-	if (*s == '\0')
-		return (len);
+	int len = 0;
 
+	if (*s == ('\0'))
+	{
+		return (len);
+	}
+	else
+	{
 	len = len + 1;
 
-	return len + _strlen_recursion(s + 1);
+	return (len + _strlen_recursion(s + 1));
+	}
 }
