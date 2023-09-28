82a6b7b869aaa576175b8b9100c9a8e57a7830ea
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-28 12:25:55 -0700

    0x08

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,11 +11,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		putchar(*s);
-		_print_rev_recursion(s - 1);
-	}
-	else
-		putchar('\0');
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	putchar(*s);
 }
