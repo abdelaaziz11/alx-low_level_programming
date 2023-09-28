8500200be50bbc0c6ec8cfc1fc15f3b3f8a31e0c
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-09-28 07:21:39 -0700

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,9 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-
+	if (*s)
+	{
+		putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
 		putchar('\n');
-	printf("%c", *s);
-	_puts_recursion(++s);
 }
