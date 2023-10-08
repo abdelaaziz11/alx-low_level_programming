1a35a3d92f3a987a056148564fcd7955ac4b921a
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-08 11:56:02 -0700

    0x0C

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,88 +1,67 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * _puts - prints a string
- * @str: pointer
- * Return: void
-*/
-
-void _puts(char *str)
+  * _isdigit - tells if the string consists of digits
+  * @argv: pointer to current item in argument
+  * Return: return 0 if all digits, 1 if not all digits.
+  */
+int _isdigit(char *argv)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
+	i = 0;
+	while (argv[i])
 	{
-		putchar(str[i]);
+		if (argv[i] >= '0' && argv[i] <= '9')
+			i++;
+		else
+			return (1);
+	}
+	return (0);
+}
+/**
+  * _atoi - converts a string of ascii digits to the values they represent
+  * @s: pointer to the source string
+  * Return: value of digits
+  */
+int _atoi(char *s)
+{
+	int i, result;
+
+	i = result = 0;
+	while (s[i])
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
 		i++;
 	}
+	return (result);
 }
-
 /**
- * _atoi - convert astring to int
- * @c: character
- * Return: int converted
-*/
-
-int _atoi(const char *c)
+  * main - main function call
+  * @argc: argument count
+  * @argv: 2D array of arguments
+  * Return: return 0 on success, 98 on failure
+  */
+int main(int argc, char *argv[])
 {
-	int p = 1;
-	unsigned long int res = 0, num, i;
-
-	for (num = 0; !(c[num] >= 48 && c[num] <= 57); num++)
-	{
-		if (c[num] == '-')
-		{
-			p *= -1;
-		}
-	}
-
-	for (i = num; c[i] >= 48 && c[i] <= 57; i++)
-	{
-		res *= 10;
-		res += (c[i] - 48);
-	}
-
-	return (p * res);
-}
-
-/**
- * print_integer - prints an integer
- * @m: intreger
- * Return: 0
-*/
-
-void print_integer(unsigned long int m)
-{
-	unsigned long int div = 1, i, res;
-
-	for (i = 0; m / div > 9; i++, div *= 10)
-		;
-
-	for (; div >= 1; m %= div, div /= 10)
-	{
-		res = m / div;
-		putchar('0' + res);
-	}
-}
-
-/**
- * main - print multiplies two positive numbers
- * @argc: integer
- * @argv: lest pointer to point
- * Return: 0
- */
-
-int main(int argc, char const *argv[])
-{
-	(void)argc;
+	int i;
 
 	if (argc != 3)
 	{
-		puts("Error ");
+		printf("Error\n");
 		exit(98);
 	}
-	print_integer(_atoi(argv[1]) * _atoi(argv[2]));
-	putchar('\n');
+	for (i = 1; i < argc; i++)
+	{
+		if (_isdigit(argv[i]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+	}
 	return (0);
 }
