748b9f4688bef727f39c33342a995967540df93f
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-10 08:29:19 -0700

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef STRUCT_DOG_H
+#define STRUCT_DOG_H
+/**
+ * struct dog - Define a new type struct dog
+ * @name: input of name
+ * @age: input of age
+ * @owner: input of owner
+ * Return: nothing
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* STRUCT_DOG_H */
