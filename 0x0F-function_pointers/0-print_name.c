4375b48f654b696f1a6336b69d6be42dfb36dc67
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-11 15:26:58 -0700

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,11 +5,11 @@
  * print_name - prints a name
  * @name: string input of name
  * @f: function pinter
- * Return: name
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (name && f)
 	f(name);
-	return (name);
 }
