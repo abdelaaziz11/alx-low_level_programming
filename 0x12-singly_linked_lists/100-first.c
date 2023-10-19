e1488c203196f5a5d2ed0b8a148c7f5b1dd8e914
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-19 13:36:00 -0700

    print text of function before the main funtion executed  task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,13 @@
 void my_constructor_fun(void) __attribute__((constructor));
 
 /**
- * main - Prints text of function before main function executed
+ * my_constructor_fun - Prints text of function before main function executed
  *
  * Return: void
  */
 
 void my_constructor_fun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
