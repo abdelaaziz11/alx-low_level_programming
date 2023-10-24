08746b32965531690968b8aec410305bad3e6ede
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-24 06:36:43 -0700

    sum all data(n) task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * sum_listint - returns the sum of all data (n)
+ * @head: pointer list
+ * Return: sum of data
+ */
+
+int sum_listint(listint_t *head)
+{
+	listint_t *current = head;
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (current->next != NULL)
+		{
+			current = current->next;
+			sum += current->n;
+		}
+	}
+	return (sum);
+}
