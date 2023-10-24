5ba65cc09d23148700d49bc9669278ad0b34f477
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-24 10:12:22 -0700

    reverse list task 11

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,11 +12,11 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prouvis = NULL;
 	listint_t *current = *head;
+	listint_t *next;
 
 	while (current != NULL)
 	{
-		listint_t *next = current->next;
-
+		next = current->next;
 		current->next = prouvis;
 		prouvis = current;
 		current = next;
