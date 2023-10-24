c91c7cb2f535d40e1f0b5452cd28536de90e89ac
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-24 12:46:34 -0700

    free listint safe task 13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,26 +1,62 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <stddef.h>
+
 /**
- * free_listint_safe - frees a listint_t list
- * @h: poiter to pointer
- * Return; size of the list
+ * find_listint_loop - finds a loop
+ * @head: poiter list
+ * Return: address of node, NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *next, *last;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (last = head->next; last != NULL; last = last->next)
+	{
+		if (last == last->next)
+			return (last);
+		for (next = head; next != last; next = next->next)
+			if (next == last->next)
+				return (last->next);
+	}
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - frees a listint list
+ * @h: pointer to pointer list
+ * Return: number of nodes
  */
 size_t free_listint_safe(listint_t **h)
 {
-	size_t size = 0;
-	listint_t *current, *tmp;
+	listint_t *p, *l_node;
+	size_t length;
+	int l = 1;
 
-	current = *h;
+	if (h == NULL || *h == NULL)
+		return (0);
 
-	while (current != NULL)
+	l_node = find_listint_loop(*h);
+	for (length = 0; (*h != l_node || l) && *h != NULL; *h = p)
 	{
-		tmp = current->next;
-		free(current);
-		size++;
-		current = tmp;
+		length++;
+		p = (*h)->next;
+		if (*h == l_node && l)
+		{
+			if (l_node == l_node->next)
+			{
+				free(*h);
+				break;
+			}
+			length++;
+			p = p->next;
+			free((*h)->next);
+			l = 0;
+		}
+		free(*h);
 	}
 	*h = NULL;
-	return (size);
+	return (length);
 }
