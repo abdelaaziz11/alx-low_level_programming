c43d6b183ad8f897f35361922996e402308ac42e
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-26 07:11:33 -0700

    task 0 convert a binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,10 +16,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = (len - 1); i >= 0; i--)
 	{
-		if (b[i] == '1')
-			total += dec_val;
-		else if (b[i] != '1' && b[i] != '0')
+		if (b[i] != '1' && b[i] != '0')
 			return (0);
+		else if (b[i] == '1')
+			total += dec_val;
 		dec_val *= 2;
 	}
 	return (total);
