9da12df25e4a0809a3ca80d19c3dfbb9f84f4056
abdelaaziz11 <abdelkhouda055@gmail.com>
2023-10-30 17:05:44 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#define USAGE "Usage: file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
 /**
  * main - copies the content of a file to another file
@@ -16,25 +20,25 @@ int main(int ac, char *av[])
 	ssize_t bytes_read;
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: file_from file_to\n"), exit(97);
+		dprintf(STDERR_FILENO, USAGE), exit(97);
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
 		if (write(fd_to, buffer, bytes_read) != bytes_read)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
 	}
 	if (bytes_read == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 	fd_from = close(fd_from);
 	fd_to = close(fd_to);
 	if (fd_from)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_from), exit(100);
 	if (fd_to)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDERR_FILENO, ERR_NOCLOSE, fd_to), exit(100);
 	return (EXIT_SUCCESS);
 }
