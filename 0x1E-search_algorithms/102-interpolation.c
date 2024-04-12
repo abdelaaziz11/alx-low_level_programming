dce69f987efa7aa5008585179f0b983857d14644
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-12 12:30:24 -0700

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -13,13 +13,13 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t pos, low, high;
 	double varf;
 
-	if (array == NULL || size == NULL)
+	if (!array)
 		return (-1);
 
 	low = 0;
 	high = size - 1;
 
-	while (size != NULL)
+	while (size)
 	{
 		varf = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
 		pos = (size_t)(low + varf);
