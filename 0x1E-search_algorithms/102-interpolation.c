40c054c30ade955a1ee44dc7c4da4bb9210b83d2
abdelaaziz11 <abdelkhouda055@gmail.com>
2024-04-12 12:21:47 -0700

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -2,24 +2,24 @@
 
 /**
  * interpolation_search - searches for a value in a sorted array of integers
- * searches for a value in a sorted array of integers
  * @array: the array
  * @size: array size
  * @value: the value
  * Return: index of the number or -1 if Null
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos, low, high;
 	double varf;
 
-	if (!array)
+	if (array == NULL || size == NULL)
 		return (-1);
 
 	low = 0;
 	high = size - 1;
 
-	while (size)
+	while (size != NULL)
 	{
 		varf = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
 		pos = (size_t)(low + varf);
